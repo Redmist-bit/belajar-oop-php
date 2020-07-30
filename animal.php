d19eff43d8607718df9b1bf11cb02729f99f4468
Redmist-bit <bayuadeirawan79@gmail.com>
2020-07-30 20:41:13 +0800

    create property

--- animal.php
+++ animal.php
@@ -1,15 +1,20 @@
 <?php
-
+//class
 class Animal
 {
-    public $name = '';
+    //property
+    public $name;
+    public $legs=2 ."<br>";
+    public $cold_blooded='false' ."<br>";
+    
+    //metod
+    function __construct($name){
+        $this -> name = $name . "<br>";
+        
+    }
 }
 
-$sheep = new Animal("shaun");
 
-echo $sheep->name; // "shaun"
-echo $sheep->legs; // 2
-echo $sheep->cold_blooded // false
 
-// NB: Boleh juga menggunakan method get (get_name(), get_legs(), get_cold_blooded())
+
 ?>
