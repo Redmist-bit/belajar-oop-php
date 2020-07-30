9cecaa1c6a1fd93f2cc452059aa63bcd95b320ab
Redmist-bit <bayuadeirawan79@gmail.com>
2020-07-30 20:03:00 +0800

    create class

---
+++ animal.php
@@ -0,0 +1,15 @@
+<?php
+
+class Animal
+{
+    public $name = '';
+}
+
+$sheep = new Animal("shaun");
+
+echo $sheep->name; // "shaun"
+echo $sheep->legs; // 2
+echo $sheep->cold_blooded // false
+
+// NB: Boleh juga menggunakan method get (get_name(), get_legs(), get_cold_blooded())
+?>
