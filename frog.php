9ba2d98aaf8d6caf92cc87e704cac93b0c942498
Redmist-bit <bayuadeirawan79@gmail.com>
2020-07-31 00:40:59 +0800

    fixed code frog class

--- frog.php
+++ frog.php
@@ -5,6 +5,9 @@ require_once 'animal.php';
 class Frog extends Animal{
     public function jump(){
         echo "Hop hop" . "<br>";
+        
     }
+    //$this->legs=4 . "<br>";
+    public $legs=4 ."<br>";
 }
 ?>
