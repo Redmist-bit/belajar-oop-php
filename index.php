355dc224999c6686d716458c5bc8b682a1deca81
Redmist-bit <bayuadeirawan79@gmail.com>
2020-07-31 00:41:28 +0800

    fixed

--- index.php
+++ index.php
@@ -13,13 +13,20 @@ echo $sheep->legs; // 2
 echo $sheep->cold_blooded; // false
 
 // NB: Boleh juga menggunakan method get (get_name(), get_legs(), get_cold_blooded())
-echo "<br>"."Release 0"."<br>";
+echo "<br>"."Release 1"."<br>";
 
 $sungokong = new Ape("kera sakti");
-echo $sungokong->name;
+//hapus coment jika ingin menjalankan
+/*echo $sungokong->name; // kera sakti
+echo $sungokong->legs; // 2 mengikuti parent class*/
 $sungokong->yell();// "Auooo"
 
+echo "<br>";
+
 $kodok = new Frog("buduk");
+//hapus coment jika ingin menjalankan
+/*echo $kodok->name; // buduk
+echo $kodok->legs;// 4, karna telah diset ulang pada child class/ class frog*/
 $kodok->jump(); // "hop hop"
 
 ?>
